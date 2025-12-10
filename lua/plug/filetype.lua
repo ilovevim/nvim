fc05212fd536312ea3517aeeb49d9aea48f0e367
ilovevim <172064319@qq.com>
2025-12-10 09:28:07 +0800

    feat(pandoc): convert current markdown file to docx

--- lua/plug/filetype.lua
+++ lua/plug/filetype.lua
@@ -63,3 +63,26 @@ vim.api.nvim_create_autocmd("FileType", {
 		end, { buffer = ev.buf, desc = "toggle public/private function" })
 	end,
 })
+
+-- markdown文件配置
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	group = filetype_group,
+	callback = function(ev)
+		-- pandoc转换为docx
+		vim.keymap.set("n", "<leader>mp", function()
+			local md_path = vim.fn.expand("%:p") -- 当前文件完整路径
+			local docx_path = vim.fn.expand("%:p:r") .. ".docx" -- 同目录同文件名.docx
+
+			local cmd = string.format('pandoc "%s" -o "%s"', vim.fn.shellescape(md_path), vim.fn.shellescape(docx_path))
+			local exit_code = vim.fn.system(cmd)
+
+			-- 检查命令是否成功
+			if vim.v.shell_error == 0 then
+				vim.notify("已生成Word文档: " .. docx_path, vim.log.levels.INFO)
+			else
+				vim.notify("转换Word失败: " .. exit_code, vim.log.levels.ERROR)
+			end
+		end, { buffer = ev.buf, desc = "misc: [p]andoc" })
+	end,
+})
