a112daa879f5a7a5976408ac2f88f26fdbd04deb
ilovevim <172064319@qq.com>
2026-03-30 17:56:12 +0800

    fix(fittencode): switch custom keymaps to default

--- init.lua
+++ init.lua
@@ -763,27 +763,8 @@ local plugins = {
 				-- event = "VeryLazy",
 				event = { "BufReadPre", "BufNewFile" },
 				opts = {
-					use_default_keymaps = false,
 					-- Default keymaps
-					keymaps = {
-						inline = {
-							["<A-o>"] = "accept_all_suggestions",
-							["<C-Down>"] = "accept_line",
-							["<C-Right>"] = "accept_word",
-							["<C-Up>"] = "revoke_line",
-							["<C-Left>"] = "revoke_word",
-							["<A-i>"] = "triggering_completion",
-						},
-						chat = {
-							["q"] = "close",
-							["[c"] = "goto_previous_conversation",
-							["]c"] = "goto_next_conversation",
-							["c"] = "copy_conversation",
-							["C"] = "copy_all_conversations",
-							["d"] = "delete_conversation",
-							["D"] = "delete_all_conversations",
-						},
-					},
+					use_default_keymaps = true,
 				},
 			},
 			"xzbdmw/colorful-menu.nvim",
